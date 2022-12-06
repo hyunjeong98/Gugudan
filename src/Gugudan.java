c5be1d173339ea3c57c99e18868d6d8830d4e7b9
hyunjeong <dhdbs200@gmail.com>
2022-12-06 22:26:34 +0900

    Add user input

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
+
 public class Gugudan {
 	public static void main(String[] args) {
-		for (int i = 2; i <= 3; i++) {
-			System.out.println(i + "단");
-			for (int j = 1; j <= 9; j++) {
-				System.out.println(i + "*" + j + "=" + (i*j));
-				
+		System.out.println("출력할 단: ");
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		if (num < 2 || num > 9) {
+			System.out.println("2이상, 9이하의 값만 입력 가능함");
+		} else {
+			System.out.println(num + "단");
+			for (int i = 1; i <= 9; i++) {
+				int result = num * i;
+				System.out.println(num + "*" + i + "=" + result);
 			}
 		}
 	}
